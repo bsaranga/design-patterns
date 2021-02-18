e2321803c6e9cf979a77806aa1505b2db89bed56
GitSaranga <bsaranga@gmail.com>
2021-02-18 12:07:56 +0530

    Interface segregation example using a segregated interfaces for each functions different birds do

---
+++ interface-segregation/Classes/Penguin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interface_segregation.Classes
+{
+    public class Penguin : IWalkable, IEatable
+    {
+        public void Eat(string food)
+        {
+            Console.WriteLine($"The penguin eats {food}");
+        }
+
+        public void Walk(int distance)
+        {
+            Console.WriteLine($"The penguin walks {distance} miles");
+        }
+    }
+}
