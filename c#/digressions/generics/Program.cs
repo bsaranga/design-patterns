93612c7fde4f584342d0d1497968c31e88f8a7f0
GitSaranga <bsaranga@gmail.com>
2021-02-22 08:27:17 +0530

    generics

---
+++ c#/digressions/generics/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GClass<int, string, float> gClass = new GClass<int, string, float>(4, "Hello", 3.14f);
+            Console.WriteLine(gClass.GetType());
+
+            gClass.SetLife(5.612f);
+            gClass.SetLife("sjhdfkj");
+            gClass.SetLife(42);
+        }
+    }
+}
